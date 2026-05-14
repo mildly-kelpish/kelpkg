57108ad460ee3eae38967fe841fe0fc78b388679
kelp <mildly_kelpish@proton.me>
2026-05-14 11:28:43 -0500

    Add files via upload

--- src/kelpk_g.nim
+++ src/kelpk_g.nim
@@ -1,5 +1,5 @@
-import commandant, strformat, std/os, parsetoml, std/envvars, std/httpclient
 let version: string = "0.1.0"
+import commandant, strformat, std/os, parsetoml, std/httpclient, std/strutils, std/sequtils  
 proc helpMessage(): string =
   result = """
   kelpkg user package manager
@@ -8,6 +8,8 @@ proc helpMessage(): string =
   help - prints a this help message and exits
   install - installs a package to ~/.kelpkg or KPDIR envvar (provided as argument)
   list - lists packagelist (see config)
+  delete - deletes package (provided as argument)
+  locinstall - installs a package from a file (remember to put your file extensions in!)
   """
 commandline:
   exitoption("help", "h", helpMessage())
@@ -16,14 +18,23 @@ commandline:
   flag(lissst, "list", "l")
   subcommand delete, "delete", "d":
     argument(pckage, string) # nim extension got mad at me for declaring a variable thrice so guess il have to do with this
-  subcommand installocal, "locinstall", "l":
+    flag(force, "force", "f")
+  subcommand installocal, "locinstall", "o":
     argument(packge, string)
   flag(update, "update", "u")
 
-# todo: figure out how to handle dependencies   i think
+
 
 echo(fmt"KELPKG {version}")
 echo("we are not responsible for any damages to your pc!")
+echo("note: dependencies are NOT handled by kelpkg")
+
+#better gaps between segments mean better readable code
+
+
+
+
+
 
 if install:
   var client = newHttpClient()
@@ -34,13 +45,14 @@ if install:
     var pbis = pbi["package"]["source"].getStr()
     var pbib = pbi["package"]["command"].getStr()
     var pbin = pbi["package"]["finbin"].getStr()
-    discard execShellCmd(fmt"git clone {pbis} /tmp/{package}")  
-    discard execShellCmd(fmt"cd /tmp/{package} && {pbib}")
-    if existsEnv("KPDIR"):
-      var dire = getEnv("KPDIR")
-      discard execShellCmd(fmt"mv /tmp/{package}/{pbin} {dire}/{package}")
-    else:
-      var defaultpat = expandTilde("~/.kelpkg") 
+    var pbiv = pbi["meta"]["version"].getFloat()
+    echo(&"would you like to install {package} {pbiv} from {pbis} (y/n) >")
+    var defaultpah = expandTilde("~/.config/kelpkg")  
+    var defaultpat = expandTilde("~/.kelpkg")
+    var confirmation = readline(stdin)
+    if confirmation == "y":
+      discard execShellCmd(fmt"git clone {pbis} /tmp/{package}")  
+      discard execShellCmd(fmt"cd /tmp/{package} && {pbib}")
       if dirExists(defaultpat):
         echo("default install dir exists and 'KPDIR' is not defined, moving binary...")
       else:
@@ -48,49 +60,211 @@ if install:
         echo("i reccommend adding /home/{user}/.kelpkg/ to PATH OR setting the environment variable 'KPDIR' to a different path\nthat is also on your PATH, that can also be written to without sudo")
       discard execShellCmd(fmt"mv /tmp/{package}/{pbin} ~/.kelpkg/{package}")
       echo(fmt"{package} installed to ~/.kelpkg/{package}")
-      var defaultpah = expandTilde("~/.config/kelpkg")  
+        
       if dirExists(defaultpah):
         echo("config dir exists, writing to package list...")
       else:
         discard execShellCmd(fmt"mkdir {defaultpah}")
-      if fileExists(fmt"{defaultpah}/packagelist.txt"):
-        let pkgl = open(fmt"{defaultpah}/packagelist.txt")
-        pkgl.writeline(package)
-        pkgl.close()
+      if fileExists(fmt"{defaultpah}/packagelist"):
+        #!package list writing here here!
+        let unsplit = readFile(fmt"{defaultpah}/packagelist")
+        var splilt = splitlines(unsplit)
+        splilt.insert(fmt"{package}-{pbiv}", 0)
+        var spilt = deduplicate(splilt)
+        var final = join(spilt, "\n")
+
+        writeFile(fmt"{defaultpah}/packagelist", final)
+        echo("Package installed succesfullly!")
       else:
         echo("packagelist does not exist, making packagelist...")
-        writeFile(fmt"{defaultpah}/packagelist.txt", package)
+        writeFile(fmt"{defaultpah}/packagelist.txt", fmt"{package}-{pbiv}")  
+        echo("Package installed succesfullly!")
+    else:
+      echo("package installation canceled")      
 
   except:
-    echo("well, something failed somewhere, likely in the getting of the package build instructions, sorry!")
+    stderr.writeLine(getCurrentExceptionMsg())
   finally:
     echo("closing http client...")
     client.close()      
+
+
+
+
+
+
+
 
 if lissst: 
   var defaultpah = expandTilde("~/.config/kelpkg")  
   if dirExists(defaultpah):
     echo("config dir exists, reading package list...")
-    if fileExists(fmt"{defaultpah}/packagelist.txt"):
-        let pkgl = readFile(fmt"{defaultpah}/packagelist.txt")
+    if fileExists(fmt"{defaultpah}/packagelist"):
+        let pkgl = readFile(fmt"{defaultpah}/packagelist")
         echo(pkgl)
     else:
       echo("packagelist does not exist!")
   else:
     echo("config dir does not exist and as such the packagelist does not")
+
+
+
+
+
+
+
 if delete:
-  var defaultpah = expandTilde("~/.config/kelpkg")  
-  if dirExists(defaultpah):
-    echo("config dir exists, reading package list...")
-    var defaultpat = expandTilde("~/.kelpkg") 
-    if fileExists(fmt"{defaultpah}/packagelist.txt"):
-      echo("i dont actually know how to remove one specific string and line so just go into your packagelist and delete it please thank you")
-      removeFile(fmt"{defaultpat}/{pckage}")
+  var defaultpah = expandTilde("~/.config/kelpkg")
+  var defaultpat = expandTilde("~/.kelpkg") 
+  if force:
+    echo("force passed, deleting package without modifying packagelist")
+    removeFile(fmt"{defaultpat}/{pckage}")
+  else:  
+    echo(fmt"would you like to delete {pckage} (y\n) >")
+    var confirmation = readline(stdin)
+    if confirmation == "y":
+
+      if dirExists(defaultpah):
+        echo("config dir exists, reading package list...")
+
+        if fileExists(fmt"{defaultpah}/packagelist"):
+          let unspliit = readFile(fmt"{defaultpah}/packagelist")
+          var spliit = splitLines(unspliit)
+          try:
+            let removeer = abbrev(pckage, spliit)
+            spliit.delete(removeer..removeer)
+            var final = join(spliit, "\n")
+            writeFile(fmt"{defaultpah}/packagelist", final)
+      
+
+            removeFile(fmt"{defaultpat}/{pckage}")
+            echo("package deleted successfully!")
+          except:
+            echo("something went wrong in modifying the packagelist, pass --force to skip packagelist checking")  
+        else:
+          echo("packagelist does not exist!")
+      else:
+        echo("config dir does not exist and as such the packagelist does not")
     else:
-      echo("packagelist does not exist!")
+      echo("package deletion canceled")   
+
+
+
+
+
+
+
+
+# fun fact! update is originally based off of the delete command  
+
+
+if update:
+  var defaultpah = expandTilde("~/.config/kelpkg")
+  var defaultpat = expandTilde("~/.kelpkg") 
+  echo(fmt"would you like to update/install all packages within your packagelist (y\n) >")
+  var confirmation = readline(stdin)
+  if confirmation == "y":
+
+    if dirExists(defaultpah):
+      echo("config dir exists, reading package list...")
+
+      if fileExists(fmt"{defaultpah}/packagelist"):
+        var client = newHttpClient()
+        let unspliit = readFile(fmt"{defaultpah}/packagelist")
+        var spliit = splitLines(unspliit)
+        for i in spliit:
+          if i == "":
+            echo("updating")
+          else:  
+            try:
+              var tempv = i.split('-') # 500 mutable variables
+              var tempest = tempv[0]
+              var verison = parseFloat(tempv[1])
+              var pbi = parsetoml.parseString(client.getContent(fmt"https://raw.githubusercontent.com/mildly-kelpish/kelpkg-repo-main/refs/heads/main/{tempest}.toml"))
+              var pbis = pbi["package"]["source"].getStr()
+              echo(pbis)
+              var pbib = pbi["package"]["command"].getStr()
+              var pbin = pbi["package"]["finbin"].getStr()
+              var pbiv = pbi["meta"]["version"].getFloat()
+              if pbiv > verison:
+                discard execShellCmd(fmt"git clone {pbis} /tmp/{tempest}")  
+                discard execShellCmd(fmt"cd /tmp/{tempest} && {pbib}")
+                if dirExists(defaultpat):
+                  echo("default install dir exists and 'KPDIR' is not defined, moving binary...")
+                else:
+                  discard execShellCmd(fmt"mkdir {defaultpat}")
+                  echo("i reccommend adding /home/{user}/.kelpkg/ to PATH ")
+                discard execShellCmd(fmt"mv /tmp/{tempest}/{pbin} ~/.kelpkg/{tempest}")
+                echo(fmt"{package} installed to ~/.kelpkg/{package}")
+        
+                if dirExists(defaultpah):
+                  echo("config dir exists, writing to package list...")
+                else:
+                  discard execShellCmd(fmt"mkdir {defaultpah}")
+                if fileExists(fmt"{defaultpah}/packagelist"):
+                  #!package list writing here!
+                  var spliot = splitLines(unspliit)
+                  try:
+                    let removeer = abbrev(tempest, spliot)
+                    spliot.delete(removeer..removeer)
+                    spliot.insert(fmt"{tempest}-{pbiv}", 0)
+                    var final = join(spliot, "\n")
+                    writeFile(fmt"{defaultpah}/packagelist", final)
+                    echo("package updated successfully!")
+                  except:
+                    echo("something went wrong")  
+
+            except:
+              stderr.writeLine(getCurrentExceptionMsg())
+            finally:
+              client.close()    
+
+      else:
+          echo("packagelist does not exist!")
+    else:
+          echo("config dir does not exist and as such the packagelist does not")
   else:
-    echo("config dir does not exist and as such the packagelist does not")
+    echo("package updating canceled")   
+
+
 if installocal:
-  # actually make this later, im taking a break now
-  echo("temp")    
-  
+  var pbi = parsetoml.parseFile(packge)
+  #errors dont kill you anymore!
+  echo("got package build instructions succesfully i think")
+  var pbis = pbi["package"]["source"].getStr()
+  var pbib = pbi["package"]["command"].getStr()
+  var pbin = pbi["package"]["finbin"].getStr()
+  var pbiv = pbi["meta"]["version"].getFloat()
+  var pbina = pbi["meta"]["name"].getStr()
+  echo(&"would you like to install {pbina} {pbiv} from {pbis} (y/n) >")
+  var defaultpah = expandTilde("~/.config/kelpkg")  
+  var defaultpat = expandTilde("~/.kelpkg")
+  var confirmation = readline(stdin)
+  if confirmation == "y":
+    discard execShellCmd(fmt"git clone {pbis} /tmp/{pbina}")  
+    discard execShellCmd(fmt"cd /tmp/{pbina} && {pbib}")
+    if dirExists(defaultpat):
+      echo("default install dir exists and 'KPDIR' is not defined, moving binary...")
+    else:
+      discard execShellCmd(fmt"mkdir {defaultpat}")
+      echo("i reccommend adding /home/{user}/.kelpkg/ to PATH OR setting the environment variable 'KPDIR' to a different path\nthat is also on your PATH, that can also be written to without sudo")
+    discard execShellCmd(fmt"mv /tmp/{pbina}/{pbin} ~/.kelpkg/{pbina}")
+    echo(fmt"{pbina} installed to ~/.kelpkg/{pbina}")
+      
+    if dirExists(defaultpah):
+      echo("config dir exists, writing to package list...")
+    else:
+      discard execShellCmd(fmt"mkdir {defaultpah}")
+    if fileExists(fmt"{defaultpah}/packagelist"):
+      #!package list writing here here!
+      let unsplit = readFile(fmt"{defaultpah}/packagelist")
+      var splilt = splitlines(unsplit)
+      splilt.insert(fmt"{pbina}-{pbiv}", 0)
+      var spilt = deduplicate(splilt)
+      var final = join(spilt, "\n")
+
+      writeFile(fmt"{defaultpah}/packagelist", final)
+      echo("package installed succesfully!")
+  else:
+    echo("package installation canceled")      
+
