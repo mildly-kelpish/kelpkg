fcc81dcbb9ed3814fafb3830555dd38ed029d122
kelp <mildly_kelpish@proton.me>
2026-05-26 20:42:29 -0500

    Add files via upload

--- src/app.d
+++ src/app.d
@@ -7,6 +7,7 @@ import colorize : fg, color, cwriteln, cwritefln;
 import requests;
 import toml;
 import std.process;
+import std.conv;
 
 TOMLDocument PBI;
 
@@ -26,6 +27,16 @@ void main(string[] argv)
 		.option("-a, --ask", "ask when installing/removing packages")
 		.argument("[package]", "Package to install/remove")
 		.parse(argv);
+	string suudooo = runCollect("id -u");
+	if (strip(suudooo) == "0")
+	{
+		writeln("wow!"); // i really need a better way to do this...
+	}
+	else
+	{
+		cwriteln("Not running as root! package will not be installed!".color(fg.red));
+		exit = 1;
+	}
 	if (args.hasFlag("install"))
 	{
 		string packag = args.argument("package");
@@ -40,7 +51,6 @@ void main(string[] argv)
 
 		if (args.hasFlag("ask"))
 		{
-			writeln("test" ~ " tes 2t" ~ "test3");
 			asking = userInput("proceed with program installation? (y/n)");
 			switch (asking)
 			{
@@ -74,53 +84,53 @@ void main(string[] argv)
 		{
 			//writing the main of the installation command in an else! wow!
 			//local bin for distroless, normal bin otherwise
-			tryMkdir("/tmp/kpkg"); // "Just MAKE SURE this exists! If it's already there, then GREAT!" - scriptlike library README
+			tryMkdir("/etc/kpkg"); // "Just MAKE SURE this exists! If it's already there, then GREAT!" - scriptlike library README
 			auto defaultpath = environment.get("KPDIR", "/usr/local/bin");
 			cwriteln("cloning package...".color(fg.yellow));
 			string src = PBI["package"]["source"].toString();
-			string cmmd = PBI["package"]["command"].toString();
-			string finalbinal = PBI["package"]["finbin"].toString();
 			string pkgn = PBI["meta"]["name"].toString();
 			tryRemovePath("/tmp/kpkg");
-			run(format("git clone %s /tmp/kpkg", strip(src, "\u0022")));
-			run(format("cd /tmp/kpkg && %s ", strip(cmmd, "\u0022")));
-			int finalcmd = tryRun(format("mv /tmp/kpkg/%s /usr/local/bin/%s", strip(finalbinal, "\u0022"), strip(pkgn, "\u0022")));
-			// if ANY of these fail the program dies
-			tryRemovePath("/tmp/kpkg"); // less scary
+			int pkgt = cast(int) PBI["meta"]["pkgt"].integer; // for some reason, it defaults to a long
+			int finalcmd;
+			string cmmd = PBI["package"]["command"].toString();
+			string finalbinal = PBI["package"]["finbin"].toString();
+			switch (pkgt)
+			{
+			case 0:
+
+				run(format("git clone %s /tmp/kpkg", strip(src, "\u0022")));
+				run(format("cd /tmp/kpkg && %s ", strip(cmmd, "\u0022")));
+				finalcmd = tryRun(format("mv /tmp/kpkg/%s /usr/local/bin/%s", strip(finalbinal, "\u0022"), strip(
+						pkgn, "\u0022")));
+				// if ANY of these fail the program dies
+				tryRemovePath("/tmp/kpkg"); // less scary
+				break;
+			case 1:
+				run(format("cd /tmp && wget %s", strip(src, "\u0022")));
+				run(format("cd /tmp && tar -xvzf %s", strip(cmmd, "\u0022"))); // todo: add support for tar.xz as well
+				finalcmd = tryRun(format("mv /tmp/%s /usr/local/bin/", strip(finalbinal, "\u0022"))); // age is currently the only binary package and has multiple binaries, this should work fine
+				break;
+			case 2:
+				writeln(format("temp"));
+				break;
+			case 3:
+				writeln(format("temp"));
+				break;
+			case 4:
+				run(strip(cmmd, "\u0022"));
+				break;
+			case 5:
+				cwriteln("the package is broken and will not be installed".color(fg.red));
+				break;
+			default:
+				cwriteln("invalid package type".color(fg.red));
+				break;
+
+			}
+
 			if (finalcmd == 0)
 			{
 				cwriteln("program installed succesfully!".color(fg.green));
-				asking = userInput("would you like to generate a .desktop file?");
-				switch (asking)
-				{
-
-				case "n":
-					cwriteln("will not make .desktop");
-					exit = 1;
-					break;
-				case "no":
-					cwriteln("will not make .desktop");
-					exit = 1;
-					break;
-				case "yes":
-					cwriteln("making .desktop");
-					break;
-				case "y":
-					cwriteln("making .desktop");
-					break;
-				default:
-					cwriteln("response not y or n, will not make .desktop");
-					exit = 1;
-					break;
-				}
-				if (exit = 1)
-				{
-					writeln("bye!");
-				}
-				else
-				{
-					writeln("make the desktop file actually");
-				}
 			}
 			else
 			{
