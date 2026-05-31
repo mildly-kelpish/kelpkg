66041041e36fe79bc955189c8ad661bb43ea9eec
kelp <mildly_kelpish@proton.me>
2026-05-30 21:09:38 -0500

    update app.d

--- src/app.d
+++ src/app.d
@@ -1,4 +1,4 @@
-import std.stdio;
+import std.stdio; // 500 imports
 import std.string;
 import std.format;
 import scriptlike.only;
@@ -8,8 +8,10 @@ import requests;
 import toml;
 import std.process;
 import std.conv;
+import std.range;
 
 TOMLDocument PBI;
+string conteent;
 
 void main(string[] argv)
 {
@@ -27,27 +29,43 @@ void main(string[] argv)
 		.option("-a, --ask", "ask when installing/removing packages")
 		.argument("[package]", "Package to install/remove")
 		.parse(argv);
-	string suudooo = runCollect("id -u");
-	if (strip(suudooo) == "0")
-	{
-		writeln("wow!"); // i really need a better way to do this...
-	}
-	else
-	{
-		cwriteln("Not running as root! package will not be installed!".color(fg.red));
-		exit = 1;
-	}
 	if (args.hasFlag("install"))
+
 	{
-		string packag = args.argument("package");
-		string conteent = getContent(
-			format(
-				"https://raw.githubusercontent.com/mildly-kelpish/kelpkg-repo-main/refs/heads/main/%s.toml", packag))
-			.toString();
-		PBI = parseTOML(conteent);
-		cwriteln(format("installing %(%sv%)...", [
-					PBI["meta"]["name"], PBI["meta"]["version"]
-				]));
+		if (exists("/etc/kpkg/repos"))
+		{
+			auto repos = File("/etc/kpkg/repos");
+			auto repolist = repos.byLine(); //todo: make repo selection more user friendly and make sure responses are valid
+			string packag = args.argument("package");
+			foreach (line; repolist)
+			{
+				writeln(line);
+				conteent = getContent(
+					format(
+						line, packag))
+					.toString();
+			}
+
+			PBI = parseTOML(conteent);
+			writeln(format("installing %s", [
+						PBI["meta"]["name"], PBI["meta"]["version"]
+					]));
+		}
+		else
+		{
+			cwriteln("/etc/kpkg/repos does not exist!".color(fg.red));
+			exit = 1;
+		}
+		string suudooo = runCollect("id -u");
+		if (strip(suudooo) == "0")
+		{
+			writeln("wow!"); // i really need a better way to do this...
+		}
+		else
+		{
+			cwriteln("Not running as root! package will not be installed!".color(fg.red));
+			exit = 1;
+		}
 
 		if (args.hasFlag("ask"))
 		{
@@ -74,7 +92,6 @@ void main(string[] argv)
 				exit = 1;
 				break;
 			}
-
 		}
 		if (exit == 1)
 		{
@@ -83,9 +100,7 @@ void main(string[] argv)
 		else
 		{
 			//writing the main of the installation command in an else! wow!
-			//local bin for distroless, normal bin otherwise
-			tryMkdir("/etc/kpkg"); // "Just MAKE SURE this exists! If it's already there, then GREAT!" - scriptlike library README
-			auto defaultpath = environment.get("KPDIR", "/usr/local/bin");
+			auto defaultpath = environment.get("KPDIR", "/usr/local/bin"); //local bin for distroless, normal bin otherwise
 			cwriteln("cloning package...".color(fg.yellow));
 			string src = PBI["package"]["source"].toString();
 			string pkgn = PBI["meta"]["name"].toString();
@@ -104,12 +119,13 @@ void main(string[] argv)
 						pkgn, "\u0022")));
 				// if ANY of these fail the program dies
 				tryRemovePath("/tmp/kpkg"); // less scary
+
 				break;
 			case 1:
 				run(format("cd /tmp && wget %s", strip(src, "\u0022")));
 				run(format("cd /tmp && tar -xvzf %s", strip(cmmd, "\u0022"))); // todo: add support for tar.xz as well
-				finalcmd = tryRun(format("mv /tmp/%s /usr/local/bin/", strip(finalbinal, "\u0022"))); // age is currently the only binary package and has multiple binaries, this should work fine
-				break;
+				finalcmd = tryRun(format("mv /tmp/%s /usr/local/bin/", strip(finalbinal, "\u0022")));
+				
 			case 2:
 				writeln(format("temp"));
 				break;
